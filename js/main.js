69de9270a4b35bcd310f4482b9d5d04360cd8471
imansalehian <iman.i.salehian@disney.com>
2018-01-09 17:30:39 -0800

    created game logic for cards

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 //Initial stylesheet
 console.log("Up and running!");
-//Names for cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 //Display what the user flipped
-console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardOne);
+//Array to hold four cards
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+  alert("It's a match!");
+}
+ else {
+  alert ("Sorry, try again.");
+}
