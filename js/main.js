aedd7da15d3bd86a91e9d69c099165ee8f3ee7f0
imansalehian <iman.i.salehian@disney.com>
2018-01-19 12:43:11 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -30,24 +30,41 @@ var cardsInPlay = [];
 //function to check for match
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
   } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
   }
-}
+};
 
 //function to log and store flipped cards
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   //log which card user flipped
   console.log("User flipped "+cards[cardId].rank);
   //add to the cardsInPlay array
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+
+  this.setAttribute('src',cards[cardId].cardImage);
+
+  if (cardsInPlay.length===2) {
+    checkForMatch();
+    cardsInPlay = [];
+  }
+};
+//function to create board
+var createBoard = function () {
+  for (var i=0; i < cards.length; i++){
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src','images/back.png');
+  cardElement.setAttribute('data-id',i);
+
+  cardElement.addEventListener('click',flipCard);
+
+  document.getElementById('game-board').appendChild(cardElement);
+ }
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 //call check for match
 checkForMatch();
